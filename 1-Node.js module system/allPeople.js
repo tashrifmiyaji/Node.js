c5a2ba0aaf45985b44f4a808b062c30e286b8d22
tashrif miyaji <tashrifmiyaji@gmail.com>
2023-11-01 22:12:18 +0600

    1-Node.js module system

---
+++ 1-Node.js module system/allPeople.js
@@ -0,0 +1,11 @@
+let people = ["sakib", "tamim", "mushfiq"];
+let addition = 10 + 10;
+function test(x) {
+    return x * x;
+}
+
+module.exports = {
+    people,
+    add: addition,
+    test,
+};
---
+++ 1-Node.js module system/index.js
@@ -0,0 +1,7 @@
+// console.log(__dirname);
+// console.log(__filename);
+let modules = require("./allPeople");
+
+console.log(modules.people);
+console.log(modules.add);
+console.log(modules.test(4));
